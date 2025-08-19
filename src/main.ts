1a5ec7c3f26277f95f77aa38ae0765f4371c4c53
shrishail21 <shrishailbhagat21@gmail.com>
2025-08-19 19:23:26 +0530

    updated the main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{cors: true});
-   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  await app.init();
 }
+
 bootstrap();
+
+export default server;
