fea47a44924b888b834b50e4be149610c26f109d
shrishailbhagat45-wq <shrishailbhagat45@gmail.com>
2025-08-19 19:51:40 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
+import * as express from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{cors: true});
-   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
+  app.enableCors({
+    origin: ['https://your-frontend.vercel.app', 'http://localhost:3000'],
+    credentials: true,
+  });
+  app.useGlobalPipes(new ValidationPipe());
+  await app.init();
 }
 
 bootstrap();
+
+export default server;
